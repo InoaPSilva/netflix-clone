459e3a66a179378877aa1daefc030c80d0e9ab14
InoaPSilva <inoapsilva@fac.pe.senac.br>
2021-04-17 19:24:57 -0300

    Netflix-Clone

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    dots: false,
+    loop:true,
+    margin:10,
+    responsive:{
+        0:{
+            items:1
+        },
+        800:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
